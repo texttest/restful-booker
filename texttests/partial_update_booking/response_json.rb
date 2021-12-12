e8e834826e6df2687fed75696913dab9a6ad5600
Emily Bache <emily@bacheconsulting.com>
2021-12-12 10:01:31 +0000

    fix bug in test data

--- texttests/partial_update_booking/response_json.rb
+++ texttests/partial_update_booking/response_json.rb
@@ -4,7 +4,7 @@
     "totalprice": 953,
     "depositpaid": true,
     "bookingdates": {
-        "checkin": "2020-08-21",
+        "checkin": "2018-05-29",
         "checkout": "2019-01-09"
     }
 }
