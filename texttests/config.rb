59acca2359be5d7f6760c9e342a1b4da6c6a6dfc
Emily Bache <emily@bacheconsulting.com>
2021-12-17 13:33:17 +0000

    make tests run in parallel, each one running the app on a unique port

--- texttests/config.rb
+++ texttests/config.rb
@@ -8,15 +8,11 @@ filename_convention_scheme:standard
 # Expanded name to use for application
 full_name:Restful Booker
 
-config_module:default
-
 link_test_path:rest_command.txt
 link_test_path:request_body.json
 link_test_path:request_headers.txt
 link_test_path:request_cookies.txt
 copy_test_path:db.json
-
-#create_catalogues:true
 
 [collate_file]
 status_code:status_code.txt
--- texttests/texttest_fixture.py
+++ texttests/texttest_fixture.py
@@ -48,7 +48,7 @@ def do_patch(full_url, headers, cookies):
 def do_delete(full_url, headers, cookies):
     return requests.delete(full_url,headers=headers, cookies=cookies)
 
-def do_request_response():
+def do_request_response(port=3001):
     if not os.path.exists("rest_command.txt"):
         sys.stderr.write("could not find rest command, exiting\n")
         sys.exit(1)
@@ -57,7 +57,7 @@ def do_request_response():
         rest, url = f.read().split()
         sys.stdout.write(f"found rest command {rest} for url {url}\n")
 
-    BASE_URL = "http://localhost:3001"
+    BASE_URL = f"http://localhost:{port}"
     full_url = f"{BASE_URL}{url}"
 
     headers = read_key_value_file("request_headers.txt")
@@ -90,15 +90,32 @@ def do_request_response():
         r = requests.get(BASE_URL + "/dumpdb", headers={"Authorization":"Basic YWRtaW46cGFzc3dvcmQxMjM="})
         print(f"requested to dump db, got response {r.status_code}")
 
-def start_server():
+def find_unique_port(minimum_port=3001):
+    """Returns a port that is:
+        - greater than or equal to the minimum_port given
+        - unique for the currently executing test case (to avoid test collisions)
+    """
+    unique_string = os.environ["TEXTTEST_SANDBOX"]
+    if unique_string is None:
+        return str(minimum_port)
+    port = abs(hash(unique_string)) % (65536 - minimum_port)
+    port += minimum_port
+    return f"{port}"
+
+def start_server(port):
     print("starting server")
     texttest_home = os.environ["TEXTTEST_HOME"]
     cwd = os.getcwd()
-    os.environ["LOAD_DB"] = "true"
-    os.environ["DB_FILE"] = os.path.join(cwd, "db.json")
+    my_env = os.environ.copy()
+    my_env["LOAD_DB"] = "true"
+    my_env["DB_FILE"] = os.path.join(cwd, "db.json")
+    my_env["PORT"] = port
+
     p = Popen([f"{texttest_home}/bin/www"],
            shell=True,
-           cwd=os.getcwd(), stdout=PIPE)
+           cwd=cwd,
+           stdout=PIPE,
+           env=my_env)
     count = 0
     while count < 3:
         msg = p.stdout.readline()
@@ -115,8 +132,9 @@ def stop_server(process):
     process.terminate()
 
 if __name__ == "__main__":
-    process = start_server()
+    port = find_unique_port()
+    process = start_server(port)
     try:
-        do_request_response()
+        do_request_response(port=port)
     finally:
         stop_server(process)
