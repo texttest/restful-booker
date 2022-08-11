5fbc7d596e9062580929d2e082f8cd2b7f54ff5c
Emily Bache <emily@bacheconsulting.com>
2022-08-11 11:43:31 +0000

    partial update booking and update booking should also collect the final db file

--- integration_tests/partial_update_booking/stdout.rb
+++ integration_tests/partial_update_booking/stdout.rb
@@ -1,3 +1,4 @@
 starting server
 found rest command PATCH for url /booking/1
+requested to dump db, got response 201
 stopping server
--- integration_tests/update_booking/stdout.rb
+++ integration_tests/update_booking/stdout.rb
@@ -1,3 +1,4 @@
 starting server
 found rest command PUT for url /booking/1
+requested to dump db, got response 201
 stopping server
