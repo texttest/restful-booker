b87178af02fbae4c5d3522690d8d42dc1a6b0f6f
Emily Bache <emily@bacheconsulting.com>
2022-12-14 13:25:44 +0000

    simplify swagger configuration

--- app.js
+++ app.js
@@ -12,22 +12,19 @@ const swaggerDocument = require('./swagger.json');
 
 var app = express();
 
-var capturemock = process.env.CAPTUREMOCK_SERVER;
+let options = {};
+const capturemock = process.env.CAPTUREMOCK_SERVER;
 if (capturemock) {
-  const requestInterceptorStr = "(req) => { req.url = req.url.replace(/http:..(127.0.0.1|localhost):[0-9]+/, '" + capturemock + "'); return req; }";
-  var options = {
-    swaggerOptions: {
-      requestInterceptor: eval(requestInterceptorStr)
-    }
-  }
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
-} else {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {}));
+    const requestInterceptorStr = "(req) => { req.url = req.url.replace(/http:..(127.0.0.1|localhost):[0-9]+/, '" + capturemock + "'); return req; }";
+    options["swaggerOptions"] = {
+        requestInterceptor: eval(requestInterceptorStr)
+    };
 }
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(xmlparser({trim: false, explicitArray: false}));
@@ -35,10 +32,10 @@ app.use(xmlparser({trim: false, explicitArray: false}));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -46,18 +43,18 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    console.log(err);
-    console.error(err.stack)
-    res.sendStatus(err.status || 500);
-  });
+    app.use(function (err, req, res, next) {
+        console.log(err);
+        console.error(err.stack)
+        res.sendStatus(err.status || 500);
+    });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  console.error(err.stack)
-  res.sendStatus(err.status || 500);
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.sendStatus(err.status || 500);
 });
 
 
