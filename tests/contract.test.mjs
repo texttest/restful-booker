6dd2d21f13fe01269af10c1fb604b04a357706cd
Hari Krishnan <harikrishnan83@gmail.com>
2024-02-19 14:22:33 +0530

    minor clean up in contract.test.js

--- tests/contract.test.mjs
+++ tests/contract.test.mjs
@@ -5,6 +5,7 @@ import { GenericContainer, Wait } from 'testcontainers';
 const APP_HOST = 'localhost';
 const APP_PORT = 3031;
 
+// SetUp
 let mongoContainer = await new GenericContainer('mongo')
   .withExposedPorts(27017)
   .withWaitStrategy(Wait.forLogMessage("Listening on"))
@@ -13,15 +14,14 @@ let mongoContainer = await new GenericContainer('mongo')
 process.env.BOOKER_DB_URL = `mongodb://${mongoContainer.getHost()}:${mongoContainer.getMappedPort(27017)}`;
 
 var initializeSeedData = (await import('../routes/seed.js')).default;
-
-console.log(initializeSeedData);
-
 await initializeSeedData();
 
 let appServer = await startAppServer(APP_PORT);
-await specmatic.testWithApiCoverage(getApp(), APP_HOST, APP_PORT);
 
+// Run Specmatic Contract Test
+await specmatic.testWithApiCoverage(getApp(), APP_HOST, APP_PORT);
 specmatic.showTestResults(test);
 
+// TearDown
 await stopAppServer(appServer);
 await mongoContainer.stop();
